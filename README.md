cba2b006b325e972734120bcaa96806674942fb1
Phil Parisi <philsbeginnercode@gmail.com>
2024-06-13 13:04:31 -0700

    excel2 test

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # ClimateSimulation
 algae pond climate simulation
+
+
+## Setup (first time)
+
+open a terminal
+`sudo apt-get install tmux`
+
+pull repo
+`cd`
+`git pull https://github.com/philparisi/climatesimulation` 
+
+make a virtual environment
+`python3 -m venv pond_venv`
+`cd ClimateSimulation`
+`source pond_venv/bin/activate`
+
+install dependencies
+`python install -e .`
+
+## RPi - Running Web App
+
+source venv and run app
+`source pond_venv/bin/activate`
+`cd ClimateSimulation`
+`flask
+
+### trigger
+
+
+## Arduino - Sending Voltages
+
+arduino sends 0-5V by default on PWM pins
+voltage multiplier doubles the arduino voltage (sends 0-10V) to Vivosun lights
---
+++ src/climatesimulation/rpi/web/excel2.py
@@ -0,0 +1,49 @@
+import pandas
+from flask import Flask, render_template, request
+from fileinput import filename
+ 
+# Flask constructor
+app = Flask(__name__)
+ 
+upload_html = """
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Upload/View Excel</title>
+</head>
+<body>
+    <h1>Upload Excel File (.xlsx)</h1>
+    <form action="{{ url_for('view') }}" method="post" enctype="multipart/form-data">
+        <input type="file" name="file"
+            accept="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-excel">
+        <input type="submit" value="Upload">
+    </form>
+</body>
+</html>
+"""
+
+# Root endpoint
+@app.get('/')
+def upload():
+    return upload_html
+ 
+ 
+@app.post('/view')
+def view():
+ 
+    # Read the File using Flask request
+    file = request.files['file']
+    # save file in local directory
+    file.save(file.filename)
+ 
+    # Parse the data as a Pandas DataFrame type
+    data = pandas.read_excel(file)
+ 
+    # Return HTML snippet that will render the table
+    return data.to_html()
+ 
+ 
+# Main Driver Function
+if __name__ == '__main__':
+    # Run the application on the local development server
+    app.run(debug=True)
